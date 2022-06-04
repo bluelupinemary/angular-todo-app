d06f867ab9492758d0d4cfe4ec298a851feeb598
bluelupinemary <marygraceaguirre@gmail.com>
2022-06-04 15:27:01 +0400

    initial commit for to do app in angular

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Task } from "../../Task";
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks : Task[] = [];
+
+  constructor(private taskservice:TaskService) { }
+
+  ngOnInit(): void {
+    this.taskservice.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks;
+    });
+  }
+
+  deleteTask(task:Task){
+    this.taskservice.deleteTask(task).subscribe(()=>{
+      this.tasks = this.tasks.filter(t => t.id !== task.id);
+    });
+  }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder;
+    this.taskservice.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskservice.addTask(task).subscribe((task)=>{
+      this.tasks.push(task);
+    });
+  }
+
+}
